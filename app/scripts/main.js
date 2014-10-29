b37ea76dcb56df658269f81b298a8e01839060a9
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-28 20:36:47 -0400

    live updated item count

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,24 +13,55 @@ this.count= 1;
 
 var totalItems=[];
 
+
+
 var item = new ToDo({
 
 });
 
-var milk= new ToDo({
-status: 'complete'
-});
+var checkForComplete= function(){
+if(item.status = 'incomplete'){
+		notDone.push(1);
+		console.log(notDone.length);
+		$('.notdone').html(notDone.length);
+}else {
+	done.push(1);
+	console.log(done.length);
+	$('.done').html(done.length);
+}
+};
 
+var buttons='<button class="delete">x</button>' + '<button class="done">:)</button>'+ '<button class="notDone">:(</button>';
 var listItem;
 
-$('button').on('click', function(event){
+
+$('#button').on('click', function(event){
 	listItem= $('.input').val();
-	event.preventDefault;	
-	console.log(listItem);
-	$('.toDoItems').append('<li>' + listItem +'</li>');
-
-// var rendered = Handlebars.templates['items'];
-
-// $('.toDoItems').append(rendered(listItem));
-// 	console.log(item);
+		event.preventDefault;	
+		console.log(listItem);
+	$('.toDoItems').append('<li class="newItem">' + listItem + buttons +'</li>');
+		totalItems.push(listItem.cout);
+	$('.count').html(totalItems.length);
  });
+
+
+$('.toDoItems').on('click', '.delete', function(){
+	$(this).parent().remove();
+		totalItems.splice(1,1);
+	$('.count').html(totalItems.length);
+});
+
+$('.toDoItems').on('click', '.done', function(){
+	item.status='complete';
+	console.log(item.status);
+});
+
+$('.toDoItems').on('click', '.notDone', function(){
+	item.status='incomplete';
+	console.log(item.status);
+});
+
+
+
+
+
